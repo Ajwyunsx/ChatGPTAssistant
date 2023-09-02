16ce08057d1fcd559be084c336c888c2cb100c1b
Ajwyunsx <107187521+Ajwyunsx@users.noreply.github.com>
2023-09-02 23:27:51 +0800

    Add files via upload

---
+++ app/src/main/java/com/cstdr/chatgpt/model/Api.java
@@ -0,0 +1,6 @@
+package com.cstdr.chatgpt.model;
+
+public interface API {
+    String OPEN_AI = "sk-Nn4MSf4TFdBSXpOQ84Tfst4JRjt4jltzXUjKj9ZB2cnHVZze"; // Open AI
+    String XF_SPEECH = "OWUzMWUwNWIxNzUwN2Y1MzAwMWY2YzY2"; // 讯飞语音
+}
